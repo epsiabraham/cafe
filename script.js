0872ac11a21b6a166ab94d5502d18edfc9c2d958
epsiabraham <epsiabraham05@gmail.com>
2026-03-18 23:27:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+  document.getElementById(id).scrollIntoView({
+    behavior: 'smooth'
+  });
 }
 
 function sendMessage() {
